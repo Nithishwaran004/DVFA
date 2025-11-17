2392f5d21e61ab39ace0405324ed89413f425f53
Nithishwaran <nithisram28@gmail.com>
2025-11-18 02:23:30 +0530

    Apply CodeGuardian remediation for Security Vulnerability: python.flask.security.audit.app-run-param-config.avoid_app_run_with_bad_host

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app.config['SECRET_KEY'] = "suchSecret"
 
 from routes import *
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000, ssl_context='adhoc')
+if __name__ == "__main__":
+    app.run(debug=False, host="127.0.0.1", port=5000)
