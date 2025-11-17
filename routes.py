a1a8e52dd1987d02a60cfbf1cef6d02e6e0d66ad
Nithishwaran <nithisram28@gmail.com>
2025-11-17 21:20:08 +0530

    Apply CodeGuardian remediation for SQL/NoSQL Injection (Injection Flaws): python.django.security.injection.ssrf.ssrf-injection-requests.ssrf-injection-requests

--- routes.py
+++ routes.py
@@ -21,14 +21,16 @@ def admin():
 
 # SSRF
 @app.route('/analyzer')
+import requests
+import httpx
+
 def follow_url():
     url = request.args.get('url', '')
     if url:
-        r = requests.get(url)
+        r = httpx.get(url)
         return render_template('analyzer.html', req=r)
     else:
         return render_template('analyzer-empty-state.html')
-    """
     Prevention:
         import ipaddress
         import socket
